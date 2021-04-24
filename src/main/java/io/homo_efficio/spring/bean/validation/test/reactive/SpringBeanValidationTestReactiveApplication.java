8a39229f24896729b3c9f6f75d680328e2a356fc
Homo Efficio <homo.efficio@gmail.com>
2021-04-24 12:54:20 +0900

    Initial Commit

---
+++ src/main/java/io/homo_efficio/spring/bean/validation/test/reactive/SpringBeanValidationTestReactiveApplication.java
@@ -0,0 +1,13 @@
+package io.homo_efficio.spring.bean.validation.test.reactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBeanValidationTestReactiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBeanValidationTestReactiveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/homo_efficio/spring/bean/validation/test/reactive/SpringBeanValidationTestReactiveApplicationTests.java
@@ -0,0 +1,13 @@
+package io.homo_efficio.spring.bean.validation.test.reactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBeanValidationTestReactiveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
